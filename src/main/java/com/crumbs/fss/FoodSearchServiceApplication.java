f0c3ddefd95bd2dd04d4a4fd0c51e12846e31b6a
elijahcbrooks <elijahcbrooks@gmail.com>
2021-05-08 13:33:08 -0500

    first commmit

---
+++ src/main/java/com/crumbs/fss/FoodSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crumbs.fss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crumbs/fss/FoodSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crumbs.fss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
