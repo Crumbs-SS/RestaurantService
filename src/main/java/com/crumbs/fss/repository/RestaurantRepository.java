9740512b6d49ea504d4523dda8f940a2eb3e53c0
elijahcbrooks <elijahcbrooks@gmail.com>
2021-06-02 11:27:48 -0500

    JTN-045 refactor: Extracting duplicate queries

--- src/main/java/com/crumbs/fss/repository/RestaurantRepository.java
+++ src/main/java/com/crumbs/fss/repository/RestaurantRepository.java
@@ -11,13 +11,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    String query = "SELECT DISTINCT r FROM restaurant r JOIN r.menuItems m WHERE m.restaurant.id = r.id " +
+            "AND LOWER(m.name) LIKE LOWER(CONCAT('%', ?1, '%'))";
 
-    @Query("SELECT DISTINCT r FROM restaurant r JOIN r.menuItems m WHERE m.restaurant.id = r.id " +
-            "AND LOWER(m.name) LIKE LOWER(CONCAT('%', ?1, '%'))")
+    @Query(query)
     Page<Restaurant> findRestaurantsByMenuItem(String menuItemName, Pageable pageable);
 
-    @Query("SELECT DISTINCT r FROM restaurant r JOIN r.menuItems m WHERE m.restaurant.id = r.id " +
-            "AND LOWER(m.name) LIKE LOWER(CONCAT('%', ?1, '%'))")
+    @Query(query)
     List<Restaurant> findRestaurantsByMenuItem(String menuItemName, Sort sort);
-
 }
